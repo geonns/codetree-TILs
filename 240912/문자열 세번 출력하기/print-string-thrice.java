c22bbbfd058008c7cb4de7d9636401bcb5293726
SeongGun <kimseonggun524@gmail.com>
2024-09-12 23:19:41 +0900

    Title: 문자열 세번 출력하기, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240912/문자열 세번 출력하기/print-string-thrice.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String a;
+        Scanner sc = new Scanner(System.in);
+
+        a = sc.next();
+
+        for (int i = 0; i < 3; ++i){
+            System.out.println(a);
+        }
+    }
+}
